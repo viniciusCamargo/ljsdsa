9aba8057cbe283ad42b1d8945881e5311153107d
Vinicius Camargo <vinicius.camargo@tua.com.br>
2015-08-24 13:57:46 -0300

    Fix tabs to spaces

--- stacks.js
+++ stacks.js
@@ -1,34 +1,34 @@
 function Stack() {
 
-	var items = [];
+  var items = [];
 
-	this.push = function(element) {
-		items.push(element);
-	};
+  this.push = function(element) {
+    items.push(element);
+  };
 
-	this.pop = function() {
-		return items.pop();
-	};
+  this.pop = function() {
+    return items.pop();
+  };
 
-	this.peek = function() {
-		return items[items.length-1];
-	};
+  this.peek = function() {
+    return items[items.length-1];
+  };
 
-	this.isEmpty = function() {
-		return items.length == 0;
-	};
+  this.isEmpty = function() {
+    return items.length == 0;
+  };
 
-	this.size = function() {
-		return items.length;
-	};
+  this.size = function() {
+    return items.length;
+  };
 
-	this.clear = function() {
-		items = [];
-	};
+  this.clear = function() {
+    items = [];
+  };
 
-	this.print = function() {
-		console.log(items.toString());
-	}
+  this.print = function() {
+    console.log(items.toString());
+  }
 
 }
 
@@ -46,14 +46,14 @@ var stack = new Stack();
 // stack.print(); // 5, 8, 11, 15
 
 // //
-// //														 - top -
+// //                            - top -
 // //
-// //														   15
-// //												 11	   11
-// //										8		 8		 8
-// //		empty stack 		5		 5		 5
+// //                              15
+// //                        11    11
+// //                   8    8     8
+// //   empty stack     5    5     5
 // //
-// //														 - base -
+// //                            - base -
 // //
 
 // stack.pop();
@@ -62,34 +62,34 @@ var stack = new Stack();
 // stack.print(); // 5, 8
 
 // //
-// //								  - top -
+// //                 - top -
 // //
-// //										15		pop()
-// //										11	 					11		pop()
-// //										8		 					8		 					8
-// //		empty stack 		5		 					5		 					5
+// //                   15    pop()
+// //                   11            11    pop()
+// //                   8             8             8
+// //   empty stack     5             5             5
 // //
-// //									- base -
+// //                 - base -
 // //
 
 // /////////////////// Decimal to binary
 // function divideBy2(decNumber) {
 
-// 	var remStack = new Stack(),
-// 			rem,
-// 			binaryString = '';
+//  var remStack = new Stack(),
+//      rem,
+//      binaryString = '';
 
-// 	while (decNumber > 0) { // {1}
-// 		rem = Math.floor(decNumber % 2); // {2}
-// 		remStack.push(rem);
-// 		decNumber = Math.floor(decNumber / 2); // {4}
-// 	}
+//  while (decNumber > 0) { // {1}
+//    rem = Math.floor(decNumber % 2); // {2}
+//    remStack.push(rem);
+//    decNumber = Math.floor(decNumber / 2); // {4}
+//  }
 
-// 	while (!remStack.isEmpty()) { // {5}
-// 		binaryString += remStack.pop().toString();
-// 	}
+//  while (!remStack.isEmpty()) { // {5}
+//    binaryString += remStack.pop().toString();
+//  }
 
-// 	return binaryString;
+//  return binaryString;
 
 // }
 
