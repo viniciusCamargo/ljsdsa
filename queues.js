73629050d8b93f334ed99d5f945666af0ce01726
Vinicius Camargo <vinicius.camargo@tua.com.br>
2015-10-01 14:58:51 -0300

    til pg 140

--- queues.js
+++ queues.js
@@ -1,25 +1,25 @@
-// function Queue() {
-// 	var items = [];
+function Queue() {
+	var items = [];
 
-// 	this.enqueue = function(element) {
-// 		items.push(element);
-// 	};
-// 	this.dequeue = function() {
-// 		return items.shift();
-// 	};
-// 	this.front = function() {
-// 		return items[0];
-// 	};
-// 	this.isEmpty = function() {
-// 		return items.length == 0;
-// 	};
-// 	this.size = function() {
-// 		return items.length;
-// 	};
-// 	this.print = function() {
-// 		console.log(items.toString());
-// 	}
-// }
+	this.enqueue = function(element) {
+		items.push(element);
+	};
+	this.dequeue = function() {
+		return items.shift();
+	};
+	this.front = function() {
+		return items[0];
+	};
+	this.isEmpty = function() {
+		return items.length == 0;
+	};
+	this.size = function() {
+		return items.length;
+	};
+	this.print = function() {
+		console.log(items.toString());
+	}
+}
 
 // var queue = new Queue();
 // console.log(queue.isEmpty()); // true
@@ -82,4 +82,31 @@ var priorityQueue = new PriorityQueue();
 priorityQueue.enqueue("John", 2);
 priorityQueue.enqueue("Jack", 1);
 priorityQueue.enqueue("Camila", 1);
-priorityQueue.print();
+// priorityQueue.print();
+
+
+// // The circular queue – Hot Potato
+
+function hotPotato (nameList, num) {
+	var queue = new Queue();
+
+	for (var i=0; i<nameList.length; i++) {
+		queue.enqueue(nameList[i]);
+	}
+
+	var eliminated = '';
+	while (queue.size() > 1) {
+		for (var i=0; i<num; i++) {
+			queue.enqueue(queue.dequeue());
+		}
+		eliminated = queue.dequeue();
+		console.log(eliminated + ' was eliminated from the Hot Potato game.');
+	}
+
+	return queue.dequeue();
+}
+
+var names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl'];
+// var winner = hotPotato(names, 7);
+// console.log('The winner is ' + winner);
+
